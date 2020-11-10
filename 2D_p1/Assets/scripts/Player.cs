decd081040a15063fb7965f121f95dc95152cde6
b50905du <71679904+b50905du@users.noreply.github.com>
2020-11-10 16:04:55 +0800

    完成檔案

--- 2D_p1/Assets/scripts/Player.cs
+++ 2D_p1/Assets/scripts/Player.cs
@@ -1,12 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+    public Text textCount;
+    public int count;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門");
-        final.SetActive(true);
+
+        if (collision.name == "傳送門")
+        {
+            print("碰到傳送門");
+            final.SetActive(true);
+        }
+        if (collision.tag == "gem") 
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "gem numbers:" + count;
+        }
     }
+    
     
 }
---
+++ 2D_p1/Assets/scripts/levelmanager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelmanager : MonoBehaviour
+{
+    public void nextlevel()
+    {
+        print("下一關卡");
+        SceneManager.LoadScene("");
+    }
+    public void backmenu()
+    {
+        print("返回選單");
+        SceneManager.LoadScene("選單");
+    }
+    public void quitgame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+}
